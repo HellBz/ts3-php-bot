43e16c1992ac7abd97cfbf775a5574a3041aa021
sisa1917 <sisa1917@windowslive.com>
2014-09-11 14:48:22 +0000

    Added debug option

--- bot.php
+++ bot.php
@@ -8,57 +8,72 @@ $server_port = "9987";	//Virtual server port
 $user = "serveradmin";	//Query login username
 $pass = "***";			//Query login password
 $name = '$killBot';		//Bot's name
+$debug = true;			//When enabled will print some debug info
 /////////////////////////////////////////////////
 
+if($debug) echo("Loading framework\n");
 require_once("libraries/TeamSpeak3/TeamSpeak3.php");
 goto main;
 main:
 // connecting to the server
+if($debug) echo("Connecting\n");
 $ts3 = TeamSpeak3::factory("serverquery://".$user.":".$pass."@".$host.":".$port."/?server_port=".$server_port."&blocking=0");
 $ts3->request('clientupdate client_nickname='.$name); // setting the name
+if($debug) echo("Connected and name set\n");
 if(!$chyba_spojeni) $ts3->message("Verze 1.9 nyní lítá");
 
 $chyba_spojeni = false;
-
+if($debug) echo("Registering event\n");
 // registering event listener
 $ts3->notifyRegister("textserver");
-
+if($debug) echo("Registering callback\n");
 // registering callback function
 TeamSpeak3_Helper_Signal::getInstance()->subscribe("notifyTextmessage", "onTextmessage");
-
+if($debug) echo("Waiting for event\n");
 // waiting for event
 try {while(1) $ts3->getAdapter()->wait();}
 catch(TeamSpeak3_Transport_Exception $error) {$chyba_spojeni = true;}
 
 function fetchGroup($user)
 {
-  global $ts3;
+  global $ts3, $debug;
+  if($debug) echo("Fetching servergroup of user: ".$user."\n");
   $id_request = $ts3->request("clientfind pattern=".$user)->toString();
+  if($debug) echo("Clientfind string array:\n"); print_r($id_request); echo("\n");
   $id_result = explode(" ", $id_request);
   $id = substr($id_result['0'], 5);
+  if($debug) echo("Invoker's ID: ".$id."\n");
   $group_request = $ts3->request("clientinfo clid=".$id)->toString();
+  if($debug) echo("Group find string array:\n"); print_r($group_request); echo("\n");
   $group_result = explode(" ", $group_request);
   $servergroup = substr($group_result['21'], 20);
-  return $servergroup;  
+  if($debug) echo("User ".$user." is member of servergroup: ".$servergroup."\n");
+  return $servergroup;
 }
 
 // called function on event
 function onTextmessage(TeamSpeak3_Adapter_ServerQuery_Event $event, TeamSpeak3_Node_Host $host)
 {
-  global $ts3;    
+  global $ts3, $name, $debug;    
   $msg = $event["msg"];
   $invoker = $event["invokername"];
   
-  echo($invoker.": ".$msg."\n");
+  if($invoker != $name) {
+    
+  if($debug) echo($invoker.": ".$msg."\n");
   
+  $group = fetchGroup($invoker);
       
-  if(fetchGroup($invoker) == "6" OR fetchGroup($invoker) == "7" OR fetchGroup($invoker) == "9") { // here I check if the user has access to bot's commands
+  if($group == "6" OR $group == "7" OR $group == "9") { // here I check if the user has access to bot's commands
+  
+  if($debug) echo("User ".$invoker." passed security check\n");
   
   //if($invoker_db["client_unique_identifier"] == "UDe92xeUw1ukT46FylXjz6LbpUY=" OR $invoker_db["client_unique_identifier"] == "vvTQr1Rnf8T1vSZDMYLe56yvd8E=" OR $invoker_db["client_unique_identifier"] == "2qid9kGm+4JdPy/aLaOAisxLbsw=" OR $invoker_db["client_unique_identifier"] == "aDtB10Aj7L81TvijvrdCJNtLgzk=" OR $invoker_db["client_unique_identifier"] == "M19tKb6kTJguzYyn6pxkBrcREzc=") { 
 
  // preparing the command arguments
  
   $arguments = explode(" ", $msg);
+  if($debug) echo("Arguments:\n"); print_r($arguments);echo("\n");
     
     // commands start here
   switch ($arguments[0]) {
@@ -217,5 +232,8 @@ Příkazy $killBota:
 
 }
 }
-if($chyba_spojeni) echo "spojeni preruseno, zkousim znovu   "; goto main;
+}
+if($chyba_spojeni) {
+  if($debug) echo("Connection lost, reconnecting...\n");
+  goto main; }
 ?>
